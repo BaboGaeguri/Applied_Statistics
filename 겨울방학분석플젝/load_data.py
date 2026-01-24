5646dd40489c486bd8d6646034611963f7c745c6
Babogaeguri <psyun0323@gmail.com>
2026-01-24 23:11:02 +0900

    update

--- 겨울방학분석플젝/load_data.py
+++ 겨울방학분석플젝/load_data.py
@@ -7,7 +7,20 @@ df = pd.read_excel(file_path)
 # print(df.head())
 # print(df.info())
 
-file_path = "겨울방학분석플젝/utils/편입편출.csv"
-df_transfer = pd.read_csv(file_path)
+# file_path = "겨울방학분석플젝/utils/편입편출.csv"
+# df_transfer = pd.read_csv(file_path)
 
-print(df_transfer)
+# print(df_transfer)
+
+file_path = "겨울방학분석플젝/raw_data/KOSPI200_연말구성종목및시총/지수구성종목_2016.csv"
+df1 = pd.read_csv(file_path, encoding='cp949')
+
+file_path = "겨울방학분석플젝/data_5602_20260124.csv"
+df2 = pd.read_csv(file_path, encoding='cp949')
+
+print("\n첫 번째 데이터프레임 정보:")
+print(df1.head())
+print(df2.head())
+print("\n두 번째 데이터프레임 정보:")
+print(df1.info())
+print(df2.info())
